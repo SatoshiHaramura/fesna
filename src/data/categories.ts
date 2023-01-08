da0d825039ba6f8d591abfe495b61b3b9dd44242
Satoshi Haramura <satoshi.haramura@gmail.com>
2023-01-08 22:41:42 +0900

    カテゴリーのデータを追加

---
+++ src/data/categories.ts
@@ -0,0 +1,20 @@
+import type { Category } from '@/types';
+
+export const categoryData: Category[] = [
+  {
+    id: 1,
+    name: '動詞',
+  },
+  {
+    id: 2,
+    name: '名詞',
+  },
+  {
+    id: 3,
+    name: '形容詞',
+  },
+  {
+    id: 4,
+    name: '副詞',
+  },
+];
