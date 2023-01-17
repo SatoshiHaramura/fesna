7dbad32b47a61ef8e77656ab8e8344d48e9a4dcb
Satoshi Haramura <satoshi.haramura@gmail.com>
2023-01-17 10:08:28 +0900

    不正解アイコンのコンポーネントを作成

---
+++ src/components/IncorrectIcon.tsx
@@ -0,0 +1,9 @@
+const CorrectIcon = () => {
+  return (
+    <div className="leading-2 text-center text-4xl font-bold text-red-500 opacity-50">
+      ✗
+    </div>
+  );
+};
+
+export default CorrectIcon;
