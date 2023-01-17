3e5764b754ad80d53fbb17ee2fb982aaafaaa10a
Satoshi Haramura <satoshi.haramura@gmail.com>
2023-01-17 10:58:02 +0900

    次のレッスンを実施するボタンのコンポーネントを作成

---
+++ src/components/NextLessonButton.tsx
@@ -0,0 +1,18 @@
+import { FC } from 'react';
+
+type Props = {
+  handleClickNextLessonButton: () => void;
+};
+
+const NextLessonButton: FC<Props> = ({ handleClickNextLessonButton }) => {
+  return (
+    <button
+      onClick={handleClickNextLessonButton}
+      className="m-1 w-40 rounded-r-full border border-gray-300 bg-gray-100 py-2 px-1 font-sans text-xl font-thin text-gray-800 hover:bg-opacity-10"
+    >
+      次のレッスン
+    </button>
+  );
+};
+
+export default NextLessonButton;
