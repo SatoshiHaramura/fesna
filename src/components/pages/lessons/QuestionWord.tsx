b8c6f597409768dc7e083c4a7c4634ee316dbc47
Satoshi Haramura <satoshi.haramura@gmail.com>
2023-02-09 14:35:51 +0900

    正誤判定結果の○×マークの実装を変更

--- src/components/pages/lessons/QuestionWord.tsx
+++ src/components/pages/lessons/QuestionWord.tsx
@@ -7,24 +7,27 @@ type Props = {
 
 const QuestionWord: FC<Props> = ({ questionWord, judgedAnswer }) => {
   const addedStyle: string[] = [];
-  const unansweredStyle = ``;
-  const correctStyle = `before:content-['○'] before:text-sky-500`;
-  const incorrectStyle = `before:content-['✗'] before:text-red-500`;
+  const correctStyle = `text-sky-500`;
+  const incorrectStyle = `text-red-500`;
+  let correctMarkOrIncorrectMark;
 
   if (judgedAnswer === undefined) {
-    addedStyle.push(unansweredStyle);
+    correctMarkOrIncorrectMark = '';
   } else if (judgedAnswer) {
     addedStyle.push(correctStyle);
+    correctMarkOrIncorrectMark = '○';
   } else {
     addedStyle.push(incorrectStyle);
+    correctMarkOrIncorrectMark = '✗';
   }
 
   return (
-    <div
-      className={`${addedStyle} relative before:absolute before:top-6 before:left-0 before:right-0 before:z-10 before:m-auto before:h-40 before:w-40 before:text-center before:text-9xl before:font-bold before:opacity-50`}
-    >
-      <div className="flex h-44 w-80 items-center justify-center rounded-xl border border-gray-100">
-        <div className="font-sans text-5xl text-gray-700">{questionWord}</div>
+    <div className="relative flex h-44 w-80 items-center justify-center rounded-xl border border-gray-100">
+      <div className="font-sans text-5xl text-gray-700">{questionWord}</div>
+      <div
+        className={`${addedStyle} z-1 absolute top-5 left-0 right-0 bottom-0 m-auto text-center text-9xl font-bold opacity-50`}
+      >
+        {correctMarkOrIncorrectMark}
       </div>
     </div>
   );
