b4af529b22332bca0e5d58e19d7eabe5ca50337e
Satoshi Haramura <satoshi.haramura@gmail.com>
2023-02-22 20:18:54 +0900

    単体テストおよび統合テストのテスト対象ファイルを指定

--- jest.config.js
+++ jest.config.js
@@ -16,6 +16,10 @@ const customJestConfig = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
+  testMatch: [
+    '<rootDir>/tests/integration/**/*.{tsx,ts}',
+    '<rootDir>/tests/unit/**/*.{tsx,ts}',
+  ],
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
