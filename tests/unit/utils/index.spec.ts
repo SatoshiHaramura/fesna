b873ec780879cd87060324acd8527c8964af4907
Satoshi Haramura <satoshi.haramura@gmail.com>
2023-02-23 14:28:09 +0900

    utilsの関数のテストを追加

---
+++ tests/unit/utils/index.spec.ts
@@ -0,0 +1,23 @@
+import { shortenPartOfSpeech } from '@/utils';
+
+describe('shortenPartOfSpeech function', () => {
+  test('when the verb is omitted', () => {
+    expect(shortenPartOfSpeech('動詞')).toBe('動');
+  });
+
+  test('when the noun is omitted', () => {
+    expect(shortenPartOfSpeech('名詞')).toBe('名');
+  });
+
+  test('when the adjective is omitted', () => {
+    expect(shortenPartOfSpeech('形容詞')).toBe('形');
+  });
+
+  test('when the adverb is omitted', () => {
+    expect(shortenPartOfSpeech('副詞')).toBe('副');
+  });
+
+  test('unexpected case', () => {
+    expect(shortenPartOfSpeech('')).toBe('');
+  });
+});
