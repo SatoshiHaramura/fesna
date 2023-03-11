d8f4342d9c3464bf37b2748eab1e56b7dc6305c4
Satoshi Haramura <satoshi.haramura@gmail.com>
2023-03-11 17:41:40 +0900

    Noto Sans Japaneseをグローバルスタイルに追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,21 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
+import { Noto_Sans_JP } from '@next/font/google';
+
+const notoSansJapanese = Noto_Sans_JP({
+  weight: ['400'],
+  preload: false,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <style jsx global>{`
+        html {
+          font-family: ${notoSansJapanese.style.fontFamily};
+        }
+      `}</style>
       <Component {...pageProps} />
       <Analytics />
     </>
