1518c53892d05a976c533ad474925e19903f0152
Satoshi Haramura <satoshi.haramura@gmail.com>
2023-03-28 09:17:32 +0900

    OGPの設定を追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import { NextSeo } from 'next-seo';
 
 import Home from '@/components/pages/home';
 
@@ -14,8 +15,31 @@ const HomePage: NextPage = () => {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>ホーム | Stock Word</title>
+        <title>Stock Word</title>
+        <meta name="keywords" content="セキュリティ, 英単語, 学習" />
       </Head>
+      <NextSeo
+        title="Stock Word"
+        description="セキュリティに関する英文記事を読むセキュリティエンジニア向けの英単語学習サービス"
+        openGraph={{
+          url: 'https://stock-word.vercel.app',
+          title: 'Stock Word',
+          description:
+            'セキュリティに関する英文記事を読むセキュリティエンジニア向けの英単語学習サービス',
+          images: [
+            {
+              url: 'https://stock-word.vercel.app/ogp.png',
+              width: 1200,
+              height: 630,
+            },
+          ],
+          siteName: 'Stock Word',
+        }}
+        twitter={{
+          handle: '@s_haramura',
+          cardType: 'summary_large_image',
+        }}
+      />
       <Home transitToMenusPage={transitToMenusPage}></Home>
     </>
   );
