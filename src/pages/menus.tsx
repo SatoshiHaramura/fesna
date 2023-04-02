8b8118d16a6c49667733f5ae0918a28165bcde7e
Satoshi Haramura <satoshi.haramura@gmail.com>
2023-04-03 07:41:25 +0900

    メニューページからSEOの上書き設定を削除

--- src/pages/menus.tsx
+++ src/pages/menus.tsx
@@ -31,25 +31,8 @@ const MenusPage: NextPage<Props> = ({ categories }) => {
         <meta name="keywords" content="セキュリティ, 英単語, 学習" />
       </Head>
       <NextSeo
-        title="Stock Word"
-        description="セキュリティに関する英文記事を読むセキュリティエンジニア向けの英単語学習サービス"
         openGraph={{
           url: 'https://stock-word.vercel.app/menus',
-          title: 'Stock Word',
-          description:
-            'セキュリティに関する英文記事を読むセキュリティエンジニア向けの英単語学習サービス',
-          images: [
-            {
-              url: 'https://stock-word.vercel.app/ogp.png',
-              width: 1200,
-              height: 630,
-            },
-          ],
-          siteName: 'Stock Word',
-        }}
-        twitter={{
-          handle: '@s_haramura',
-          cardType: 'summary_large_image',
         }}
       />
       <Menus
