bbca5f851c8c8e544a3bb3185a1f525132d6dbb1
Satoshi Haramura <satoshi.haramura@gmail.com>
2023-04-02 10:34:56 +0900

    レッスンページにSEOの設定を追加

--- src/pages/lessons.tsx
+++ src/pages/lessons.tsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
-import Lessons from '@/components/pages/lessons';
 import { useRouter } from 'next/router';
+import { NextSeo } from 'next-seo';
+
+import Lessons from '@/components/pages/lessons';
 
 export const LessonsPage = () => {
   const router = useRouter();
@@ -11,8 +13,32 @@ export const LessonsPage = () => {
   return (
     <>
       <Head>
-        <title>レッスンページ</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>レッスン | Stock Word</title>
+        <meta name="keywords" content="セキュリティ, 英単語, 学習" />
       </Head>
+      <NextSeo
+        title="Stock Word"
+        description="セキュリティに関する英文記事を読むセキュリティエンジニア向けの英単語学習サービス"
+        openGraph={{
+          url: 'https://stock-word.vercel.app/lessons',
+          title: 'Stock Word',
+          description:
+            'セキュリティに関する英文記事を読むセキュリティエンジニア向けの英単語学習サービス',
+          images: [
+            {
+              url: 'https://stock-word.vercel.app/ogp.png',
+              width: 1200,
+              height: 630,
+            },
+          ],
+          siteName: 'Stock Word',
+        }}
+        twitter={{
+          handle: '@s_haramura',
+          cardType: 'summary_large_image',
+        }}
+      />
       <Lessons handleClickMenuButton={handleClickMenuButton} />
     </>
   );
