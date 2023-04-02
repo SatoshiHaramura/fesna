b9ad0afc27cd422bf437253a8761b7ff919f550b
Satoshi Haramura <satoshi.haramura@gmail.com>
2023-04-03 07:40:31 +0900

    トップページからSEOの上書き設定を削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import { NextPage } from 'next';
 import { useRouter } from 'next/router';
-import { NextSeo } from 'next-seo';
 
 import Home from '@/components/pages/home';
 
@@ -15,31 +14,8 @@ const HomePage: NextPage = () => {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <title>Stock Word</title>
         <meta name="keywords" content="セキュリティ, 英単語, 学習" />
       </Head>
-      <NextSeo
-        title="Stock Word"
-        description="セキュリティに関する英文記事を読むセキュリティエンジニア向けの英単語学習サービス"
-        openGraph={{
-          url: 'https://stock-word.vercel.app',
-          title: 'Stock Word',
-          description:
-            'セキュリティに関する英文記事を読むセキュリティエンジニア向けの英単語学習サービス',
-          images: [
-            {
-              url: 'https://stock-word.vercel.app/ogp.png',
-              width: 1200,
-              height: 630,
-            },
-          ],
-          siteName: 'Stock Word',
-        }}
-        twitter={{
-          handle: '@s_haramura',
-          cardType: 'summary_large_image',
-        }}
-      />
       <Home transitToMenusPage={transitToMenusPage}></Home>
     </>
   );
