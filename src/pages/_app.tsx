463557155f58b27e938ed4d559c6b3e4047420e2
Satoshi Haramura <satoshi.haramura@gmail.com>
2023-04-03 07:38:11 +0900

    全てのページにデフォルトのSEOの設定を読み込むように修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { Analytics } from '@vercel/analytics/react';
 import { Noto_Sans_JP } from '@next/font/google';
+import { DefaultSeo } from 'next-seo';
+import SEO from 'next-seo.config';
 
 const notoSansJapanese = Noto_Sans_JP({
   weight: ['400'],
@@ -16,6 +18,7 @@ export default function App({ Component, pageProps }: AppProps) {
           font-family: ${notoSansJapanese.style.fontFamily};
         }
       `}</style>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
       <Analytics />
     </>
