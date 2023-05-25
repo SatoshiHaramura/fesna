b5b6684392f5082e30c3d01f698cbb48d0715391
Satoshi Haramura <satoshi.haramura@gmail.com>
2023-05-25 14:43:58 +0900

    テスト用の属性の追加およびHTMLタグを修正

--- src/components/pages/lessons/RelatedWord.tsx
+++ src/components/pages/lessons/RelatedWord.tsx
@@ -14,14 +14,14 @@ const roboto = Roboto_Flex({
 
 const RelatedWord: FC<Props> = ({ relatedWords }) => {
   return (
-    <div className="flex flex-row gap-3 py-2">
-      <div className="m-auto w-2/12 rounded-xl border border-slate-400 bg-slate-100 px-0.5 py-2 text-center text-sm">
+    <div className="flex flex-row gap-3 py-2" data-testid="related-word">
+      <h2 className="m-auto w-2/12 rounded-xl border border-slate-400 bg-slate-100 px-0.5 py-2 text-center text-sm">
         関連語
-      </div>
-      <div className="flex w-10/12 flex-col justify-center">
+      </h2>
+      <ul className="flex w-10/12 flex-col justify-center">
         {relatedWords.map(({ partOfSpeech, word, meaning }, index) => {
           return (
-            <div key={index} className="flex flex-row py-0.5">
+            <li key={index} className="flex flex-row py-0.5">
               <div>
                 <div className="inline-block rounded border border-slate-400 px-0.5 leading-5">
                   {shortenPartOfSpeech(partOfSpeech)}
@@ -34,10 +34,10 @@ const RelatedWord: FC<Props> = ({ relatedWords }) => {
               </div>
               <div className="p-0.5 leading-4">:</div>
               <div className="p-0.5 leading-4">{meaning}</div>
-            </div>
+            </li>
           );
         })}
-      </div>
+      </ul>
     </div>
   );
 };
