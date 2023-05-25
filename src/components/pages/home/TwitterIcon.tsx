d23a49c4bfb4aa7a66af708498c20f5610404f0c
Satoshi Haramura <satoshi.haramura@gmail.com>
2023-05-25 14:45:15 +0900

    role属性およびaria-label属性を追加

--- src/components/pages/home/TwitterIcon.tsx
+++ src/components/pages/home/TwitterIcon.tsx
@@ -4,7 +4,11 @@ import { FaTwitter } from 'react-icons/fa';
 const TwitterIcon = () => {
   return (
     <Link href="https://twitter.com/s_haramura">
-      <FaTwitter className="text-xl text-gray-500 hover:text-sky-500" />
+      <FaTwitter
+        role="img"
+        aria-label="Twitter"
+        className="text-xl text-gray-500 hover:text-sky-500"
+      />
     </Link>
   );
 };
