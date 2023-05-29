400b89ea199b10e7c9079c8de4b32376e72200e8
Satoshi Haramura <satoshi.haramura@gmail.com>
2023-05-29 09:27:20 +0900

    テストにdescribeを追加

--- tests/unit/components/SelectionTypeQuestion.spec.tsx
+++ tests/unit/components/SelectionTypeQuestion.spec.tsx
@@ -5,96 +5,98 @@ import type { Question } from '@/types';
 import SelectionTypeQuestion from '@/components/pages/lessons/SelectionTypeQuestion';
 
 describe('SelectionTypeQuestion component', () => {
-  const questions: Question[] = [
-    {
-      id: 1,
-      lessonId: 1,
-      question: 'include',
-      choices: ['を完了する', 'を避ける', 'に注意を払う、と述べる', 'を含む'],
-      answer: 'を含む',
-      partOfSpeech: '動詞',
-      relatedWords: [
-        {
-          word: 'inclusion',
-          meaning: '包含、含まれるもの',
-          partOfSpeech: '名詞',
-        },
-        {
-          word: 'inclusive',
-          meaning: 'すべてを含んだ',
-          partOfSpeech: '形容詞',
-        },
-      ],
-      useExamples: [
-        {
-          example: 'the email including a malicious macro',
-          meaning: '悪意のあるマクロを含むメール',
-        },
-      ],
-    },
-    {
-      id: 2,
-      lessonId: 1,
-      question: 'steal',
-      choices: [
-        'を悪用する',
-        'を盗む',
-        'になりすます',
-        'を脅迫する、ゆすり取る',
-      ],
-      answer: 'を盗む',
-      partOfSpeech: '動詞',
-      relatedWords: [],
-      useExamples: [
-        {
-          example: 'steal sensitive information',
-          meaning: '機微な情報を盗む',
-        },
-      ],
-    },
-  ];
-  const currentQuestionNumber = 1;
-  const handleClickChoiceButton = jest.fn();
-  const judgedAnswer: boolean | undefined = undefined;
-  const automaticTextToSpeechMode = false;
-  const handleClickPlaySoundButton = jest.fn();
+  describe('display each child component', () => {
+    const questions: Question[] = [
+      {
+        id: 1,
+        lessonId: 1,
+        question: 'include',
+        choices: ['を完了する', 'を避ける', 'に注意を払う、と述べる', 'を含む'],
+        answer: 'を含む',
+        partOfSpeech: '動詞',
+        relatedWords: [
+          {
+            word: 'inclusion',
+            meaning: '包含、含まれるもの',
+            partOfSpeech: '名詞',
+          },
+          {
+            word: 'inclusive',
+            meaning: 'すべてを含んだ',
+            partOfSpeech: '形容詞',
+          },
+        ],
+        useExamples: [
+          {
+            example: 'the email including a malicious macro',
+            meaning: '悪意のあるマクロを含むメール',
+          },
+        ],
+      },
+      {
+        id: 2,
+        lessonId: 1,
+        question: 'steal',
+        choices: [
+          'を悪用する',
+          'を盗む',
+          'になりすます',
+          'を脅迫する、ゆすり取る',
+        ],
+        answer: 'を盗む',
+        partOfSpeech: '動詞',
+        relatedWords: [],
+        useExamples: [
+          {
+            example: 'steal sensitive information',
+            meaning: '機微な情報を盗む',
+          },
+        ],
+      },
+    ];
+    const currentQuestionNumber = 1;
+    const handleClickChoiceButton = jest.fn();
+    const judgedAnswer: boolean | undefined = undefined;
+    const automaticTextToSpeechMode = false;
+    const handleClickPlaySoundButton = jest.fn();
 
-  let renderResult: RenderResult;
-  beforeEach(() => {
-    renderResult = render(
-      <SelectionTypeQuestion
-        questions={questions}
-        currentQuestionNumber={currentQuestionNumber}
-        handleClickChoiceButton={handleClickChoiceButton}
-        judgedAnswer={judgedAnswer}
-        automaticTextToSpeechMode={automaticTextToSpeechMode}
-        handleClickPlaySoundButton={handleClickPlaySoundButton}
-      />
-    );
-  });
-  afterEach(() => {
-    renderResult.unmount();
-  });
+    let renderResult: RenderResult;
+    beforeEach(() => {
+      renderResult = render(
+        <SelectionTypeQuestion
+          questions={questions}
+          currentQuestionNumber={currentQuestionNumber}
+          handleClickChoiceButton={handleClickChoiceButton}
+          judgedAnswer={judgedAnswer}
+          automaticTextToSpeechMode={automaticTextToSpeechMode}
+          handleClickPlaySoundButton={handleClickPlaySoundButton}
+        />
+      );
+    });
+    afterEach(() => {
+      renderResult.unmount();
+    });
 
-  test('display the speaker toggle button', () => {
-    const speakerToggleButton = renderResult.getByTestId(
-      'speaker-toggle-button'
-    );
-    expect(speakerToggleButton).toBeInTheDocument();
-  });
+    test('display the speaker toggle button', () => {
+      const speakerToggleButton = renderResult.getByTestId(
+        'speaker-toggle-button'
+      );
+      expect(speakerToggleButton).toBeInTheDocument();
+    });
 
-  test('display the question progress', () => {
-    const questionProgress = renderResult.getByTestId('question-progress');
-    expect(questionProgress).toBeInTheDocument();
-  });
+    test('display the question progress', () => {
+      const questionProgress = renderResult.getByTestId('question-progress');
+      expect(questionProgress).toBeInTheDocument();
+    });
 
-  test('display the question word', () => {
-    const questionWord = renderResult.getByTestId('question-word');
-    expect(questionWord).toBeInTheDocument();
-  });
+    test('display the question word', () => {
+      const questionWord = renderResult.getByTestId('question-word');
+      expect(questionWord).toBeInTheDocument();
+    });
 
-  test('display the choice button list', () => {
-    const choiceButtonList = renderResult.getByTestId('choice-button-list');
-    expect(choiceButtonList).toBeInTheDocument();
+    test('display the choice button list', () => {
+      const choiceButtonList = renderResult.getByTestId('choice-button-list');
+      expect(choiceButtonList).toBeInTheDocument();
+    });
   });
 });
