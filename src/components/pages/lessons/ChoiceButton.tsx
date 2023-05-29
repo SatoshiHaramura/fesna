eef98b07dcecda558a94ed63a909e78603eba175
Satoshi Haramura <satoshi.haramura@gmail.com>
2023-05-29 13:57:54 +0900

    選択肢ボタンのコンポーネントをリファクタリング

--- src/components/pages/lessons/ChoiceButton.tsx
+++ src/components/pages/lessons/ChoiceButton.tsx
@@ -8,6 +8,26 @@ type Props = {
   correctChoiceIndex: number;
 };
 
+const commonStyle =
+  'h-20 w-80 rounded-full border py-2 px-8 text-left text-xl border-2';
+const styles = {
+  unanswered: `${commonStyle} bg-slate-100 bg-opacity-40 border-slate-300 text-gray-800`,
+  correct: `${commonStyle} bg-lime-100 border-lime-400 text-gray-800`,
+  incorrect: `${commonStyle} bg-slate-50 bg-opacity-80 border-slate-200 text-gray-400`,
+};
+
+const getStyleKey = (
+  judgedAnswer: undefined | boolean,
+  index: number,
+  correctChoiceIndex: number
+) => {
+  return judgedAnswer === undefined
+    ? 'unanswered'
+    : index === correctChoiceIndex
+    ? 'correct'
+    : 'incorrect';
+};
+
 const ChoiceButton: FC<Props> = ({
   choice,
   handleClickChoiceButton,
@@ -15,22 +35,8 @@ const ChoiceButton: FC<Props> = ({
   index,
   correctChoiceIndex,
 }) => {
-  const addedStyle: string[] = [];
-  const unansweredStyle =
-    'bg-slate-100 bg-opacity-40 border-slate-300 text-gray-800';
-  const correctStyle = `bg-lime-100 border-lime-400 text-gray-800`;
-  const incorrectStyle =
-    'bg-slate-50 bg-opacity-80 border-slate-200 text-gray-400';
-
-  if (judgedAnswer === undefined) {
-    addedStyle.push(unansweredStyle);
-  } else if (index === correctChoiceIndex) {
-    addedStyle.push(correctStyle);
-  } else if (index !== correctChoiceIndex) {
-    addedStyle.push(incorrectStyle);
-  }
-
-  const style = `${addedStyle} h-20 w-80 rounded-full border py-2 px-8 text-left text-xl border-2`;
+  const styleKey = getStyleKey(judgedAnswer, index, correctChoiceIndex);
+  const style = styles[styleKey];
 
   return (
     <button
