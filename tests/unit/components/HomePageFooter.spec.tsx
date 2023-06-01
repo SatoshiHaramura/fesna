a1e2708791f46e941d4683b97cfb1c059997bada
Satoshi Haramura <satoshi.haramura@gmail.com>
2023-06-01 13:31:42 +0900

    トップページのフッターコンポーネントのテストを修正

--- tests/unit/components/HomePageFooter.spec.tsx
+++ tests/unit/components/HomePageFooter.spec.tsx
@@ -1,4 +1,6 @@
-import { render, RenderResult } from '@testing-library/react';
+import { render, RenderResult, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { mockAllIsIntersecting } from 'react-intersection-observer/test-utils';
 
 import HomePageFooter from '@/components/pages/home/HomePageFooter';
 
@@ -6,31 +8,97 @@ describe('HomePageFooter component', () => {
   let renderResult: RenderResult;
   beforeEach(() => {
     renderResult = render(<HomePageFooter />);
+    mockAllIsIntersecting(true);
   });
   afterEach(() => {
     renderResult.unmount();
+    mockAllIsIntersecting(false);
   });
 
-  test('display the terms of use', () => {
-    const termsOfUse = renderResult.getByRole('button', { name: '利用規約' });
-    expect(termsOfUse).toBeInTheDocument();
-  });
+  describe('operations of the terms of use dialog', () => {
+    test('display the dialog when the terms of use is clicked', async () => {
+      const user = userEvent.setup();
+      const termsOfUse = renderResult.getByRole('button', { name: '利用規約' });
+      await user.click(termsOfUse);
 
-  test('display the privacy policy', () => {
-    const privacyPolicy = renderResult.getByRole('button', {
-      name: 'プライバシーポリシー',
+      const dialog = renderResult.getByRole('dialog');
+      expect(
+        within(dialog).getByRole('heading', {
+          name: '利用規約',
+        })
+      ).toBeInTheDocument();
     });
-    expect(privacyPolicy).toBeInTheDocument();
+
+    test('close the dialog when the close button is clicked', async () => {
+      const user = userEvent.setup();
+      const termsOfUse = renderResult.getByRole('button', { name: '利用規約' });
+      await user.click(termsOfUse);
+
+      const dialog = renderResult.getByRole('dialog');
+      expect(
+        within(dialog).getByRole('heading', {
+          name: '利用規約',
+        })
+      ).toBeInTheDocument();
+
+      const closeButton = within(dialog).getByRole('button', {
+        name: '閉じる',
+      });
+      await user.click(closeButton);
+
+      expect(dialog).not.toBeInTheDocument();
+    });
   });
 
-  test('display the github icon', () => {
-    const githubIcon = renderResult.getByRole('link', { name: 'GitHub' });
-    expect(githubIcon).toBeInTheDocument();
+  describe('operations of the privacy policy dialog', () => {
+    test('display the dialog when the privacy policy is clicked', async () => {
+      const user = userEvent.setup();
+      const privacyPolicy = renderResult.getByRole('button', {
+        name: 'プライバシーポリシー',
+      });
+      await user.click(privacyPolicy);
+
+      const dialog = renderResult.getByRole('dialog');
+      expect(
+        within(dialog).getByRole('heading', {
+          name: 'プライバシーポリシー',
+        })
+      ).toBeInTheDocument();
+    });
+
+    test('close the dialog when the close button is clicked', async () => {
+      const user = userEvent.setup();
+      const privacyPolicy = renderResult.getByRole('button', {
+        name: 'プライバシーポリシー',
+      });
+      await user.click(privacyPolicy);
+
+      const dialog = renderResult.getByRole('dialog');
+      expect(
+        within(dialog).getByRole('heading', {
+          name: 'プライバシーポリシー',
+        })
+      ).toBeInTheDocument();
+
+      const closeButton = within(dialog).getByRole('button', {
+        name: '閉じる',
+      });
+      await user.click(closeButton);
+
+      expect(dialog).not.toBeInTheDocument();
+    });
   });
 
-  test('display the twitter icon', () => {
-    const twitterIcon = renderResult.getByRole('link', { name: 'Twitter' });
-    expect(twitterIcon).toBeInTheDocument();
+  describe('display of the sns link icons', () => {
+    test('display the github icon', () => {
+      const githubIcon = renderResult.getByRole('link', { name: 'GitHub' });
+      expect(githubIcon).toBeInTheDocument();
+    });
+
+    test('display the twitter icon', () => {
+      const twitterIcon = renderResult.getByRole('link', { name: 'Twitter' });
+      expect(twitterIcon).toBeInTheDocument();
+    });
   });
 
   test('display the copyright', () => {
