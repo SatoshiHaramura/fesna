7d70c5a8ad1538eb0ad4b95dcf2f674684069320
Satoshi Haramura <satoshi.haramura@gmail.com>
2023-06-02 14:38:47 +0900

    不正解時のマークのテスト用の属性の値を修正

--- src/components/pages/lessons/IncorrectAnswerMark.tsx
+++ src/components/pages/lessons/IncorrectAnswerMark.tsx
@@ -2,7 +2,7 @@ const IncorrectAnswerMark = () => {
   return (
     <div
       className="leading-2 text-center font-sans text-5xl font-bold text-red-500 opacity-90"
-      data-testid="incorrect-icon"
+      data-testid="incorrect-answer-mark"
     >
       ✗
     </div>
