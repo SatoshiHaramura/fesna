461d47d38cf4f7fee3567c3800d0ade43ec1105a
Satoshi Haramura <satoshi.haramura@gmail.com>
2023-06-08 08:13:37 +0900

    テストケースの文言を修正

--- tests/unit/utils/index.spec.ts
+++ tests/unit/utils/index.spec.ts
@@ -1,19 +1,19 @@
 import { shortenPartOfSpeech } from '@/utils';
 
 describe('shortenPartOfSpeech function', () => {
-  test('when the verb is omitted', () => {
+  test('when the verb is shortened', () => {
     expect(shortenPartOfSpeech('動詞')).toBe('動');
   });
 
-  test('when the noun is omitted', () => {
+  test('when the noun is shortened', () => {
     expect(shortenPartOfSpeech('名詞')).toBe('名');
   });
 
-  test('when the adjective is omitted', () => {
+  test('when the adjective is shortened', () => {
     expect(shortenPartOfSpeech('形容詞')).toBe('形');
   });
 
-  test('when the adverb is omitted', () => {
+  test('when the adverb is shortened', () => {
     expect(shortenPartOfSpeech('副詞')).toBe('副');
   });
 });
